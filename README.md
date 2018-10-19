06ea9bec42a81df95a8a1b8e0739af60cd2a6dfe
BellergyRC <rc@bellergy.com>
2018-10-19 20:18:24 +0800

    VR-360 layout setup

--- README.md
+++ README.md
@@ -21,3 +21,18 @@ Browser will auto reload when js, scss, html changed.
 
 ## Testing page
 http://pms.dq.hk/inhouse/staffs/michael/vr-xmas/
+
+## Reference:
+https://www.youtube.com/watch?v=K09YezPzCKc
+
+## Resource:
+https://www.google.com.hk/maps/@22.2853747,114.1616804,3a,75y,220h,90t/data=!3m8!1e1!3m6!1sAF1QipP2md_1PgPYFw8dUvcwgVTjB11F9sIiCg8IgPJG!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipP2md_1PgPYFw8dUvcwgVTjB11F9sIiCg8IgPJG%3Dw520-h260-k-no!7i10000!8i5000
+
+
+https://www.google.com.hk/maps/contrib/113476076542537131572/photos/@22.3027284,114.1840749,3a,75y,250.98h,74.53t/data=!3m7!1e1!3m5!1sAF1QipN2VhmpiCiukPWghG6A3u4ih4g0XDZD-UHHkL3z!2e10!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipN2VhmpiCiukPWghG6A3u4ih4g0XDZD-UHHkL3z%3Dw365-h260-k-no-pi-0-ya87.5-ro-0-fo100!7i10000!8i5000!4m3!8m2!3m1!1e1
+
+https://svd360.istreetview.com/
+
+https://www.360cities.net/profile/wong-chi-chuen
+
+https://www.360cities.net/profile/wong-chi-chuen
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,36 @@
 /* global AFRAME */
 
+(function () {
+  if ( typeof window.CustomEvent === "function" ) return false;
+    function CustomEvent ( event, params ) {
+     params = params || { bubbles: false, cancelable: false, detail: undefined };
+     var evt = document.createEvent( 'CustomEvent' );
+     evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+     return evt;
+  }
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent;
+
+
+  // Polyfill always provides us with `navigator.getVRDisplays`
+  navigator.getVRDisplays().then(displays => {
+    // If we have a native VRDisplay, or if the polyfill
+    // provided us with a CardboardVRDisplay, use it
+    if (displays.length) {
+      vrDisplay = displays[0];
+      controls = new THREE.VRControls(camera);
+      vrDisplay.requestAnimationFrame(animate);
+    } else {
+      // If we don't have a VRDisplay, we're probably on
+      // a desktop environment, so set up desktop-oriented controls
+      controls = new THREE.OrbitControls(camera);
+      requestAnimationFrame(animate);
+    }
+  });
+})();
+
+  
+
 /**
  * Component that listens to an event, fades out an entity, swaps the texture, and fades it
  * back in.
